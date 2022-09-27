ceba39c4536b1438697e5207c2f07ae44311712c
jacksonboyett <jackson.boyett@gmail.com>
2022-09-27 17:23:25 +0200

    made number buttons functional

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let operator = 'plus';
-let num1 = 6;
-let num2 = 9;
+let operator = 'divide';
+let number = [];
 
-
-function operate(operator, num1, num2) {
-    console.log(operator)
+// ----- CALCULATE FUNCTION -----
+function calculate(operator, num1, num2) {
     if (operator == "plus") {
         answer = num1 + num2;
     } else if (operator == 'minus') {
@@ -16,3 +14,75 @@ function operate(operator, num1, num2) {
     }
     return answer; 
 };
+
+// ----- COMBINE DIGITS FUNCTION -----
+function combine(num) {
+    number.push(num);
+    joinedString = number.join('');
+    joinedNumber = Number(joinedString)
+    console.log(joinedNumber)
+    return number
+};
+
+// ----- BUTTONS OUTPUT NUMBERS -----
+let sevenButton = document.querySelector('#seven');
+sevenButton.addEventListener('mousedown', sevenFxn)
+
+function sevenFxn() {
+    combine(7);
+}
+
+let eightButton = document.querySelector('#eight');
+eightButton.addEventListener('mousedown', eightFxn)
+
+function eightFxn() {
+    combine(8);
+}
+let nineButton = document.querySelector('#nine');
+nineButton.addEventListener('mousedown', nineFxn)
+
+function nineFxn() {
+    combine(9);
+}
+
+let fourButton = document.querySelector('#four');
+fourButton.addEventListener('mousedown', fourFxn)
+
+function fourFxn() {
+    combine(4);
+}
+
+let fiveButton = document.querySelector('#five');
+fiveButton.addEventListener('mousedown', fiveFxn)
+
+function fiveFxn() {
+    combine(5);
+}
+
+let sixButton = document.querySelector('#six');
+sixButton.addEventListener('mousedown', sixFxn)
+
+function sixFxn() {
+    combine(6);
+}
+
+let oneButton = document.querySelector('#one');
+oneButton.addEventListener('mousedown', oneFxn)
+
+function oneFxn() {
+    combine(1);
+}
+
+let twoButton = document.querySelector('#two');
+twoButton.addEventListener('mousedown', twoFxn)
+
+function twoFxn() {
+    combine(2);
+}
+
+let threeButton = document.querySelector('#three');
+threeButton.addEventListener('mousedown', threeFxn)
+
+function threeFxn() {
+    combine(3);
+}
