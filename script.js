ecdb8e64dd3d68c5e800837d928da2fc33dd4e1f
jacksonboyett <jackson.boyett@gmail.com>
2022-09-29 17:22:05 +0200

    styled like Tron. Need to work out bugs

--- script.js
+++ script.js
@@ -1,88 +1,194 @@
-// let operator = 'divide';
-// let number = [];
+let operator = 'divide';
+let number = [];
+let num1 = [1,1];
+let answer = 1;
 
-// // ----- CALCULATE FUNCTION -----
-// function calculate(operator, num1, num2) {
-//     if (operator == "plus") {
-//         answer = num1 + num2;
-//     } else if (operator == 'minus') {
-//         answer = num1 - num2;
-//     } else if (operator == 'mult') {
-//         answer = num1 * num2;
-//     } else { 
-//         answer = num1 / num2;
-//     }
-//     return answer; 
-// };
 
-// // ----- COMBINE DIGITS FUNCTION -----
-// function combine(num) {
-//     number.push(num);
-//     joinedString = number.join('');
-//     joinedNumber = Number(joinedString)
-//     console.log(joinedNumber)
-//     return number
-// };
+// ----- COMBINE DIGITS FUNCTION -----
+function combine(num) {
+    if (num1[1] === 'operate') { 
+        number = [];
+        console.log(`Number array: ${number}`);
+        num1[1] = 1;
+    } else if (num1[1] === 'clear') {
+        number = [];
+        console.log(`Number array: ${number}`);
+        num1[1] = 1;
+    } 
+    number.push(num);
+    if (num1[1] === 'negative') {
+        number.pop();
+    }
+    joinedString = number.join('');
+    joinedNumber = Number(joinedString)
+    if (num1[1] === 'negative') {
+        joinedNumber = joinedNumber * -1;
+        num1[1] === 0;
+    }
+    changeDisplay(joinedNumber);
+    console.log(`Number 1: ${joinedNumber}`)
+    return joinedNumber
+};
 
-// // ----- BUTTONS OUTPUT NUMBERS -----
-// let sevenButton = document.querySelector('.seven');
-// sevenButton.addEventListener('mousedown', sevenFxn)
+// ----- OPERATER FUNCTIONS -----
+let divideButton = document.querySelector('.divide');
+divideButton.addEventListener('mousedown', divideFxn);
 
-// function sevenFxn() {
-//     combine(7);
-// }
+function divideFxn() {
+    num1 = [joinedNumber, 'operate' , 'divide'];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// let eightButton = document.querySelector('.eight');
-// eightButton.addEventListener('mousedown', eightFxn)
+let multButton = document.querySelector('.mult');
+multButton.addEventListener('mousedown', multFxn);
 
-// function eightFxn() {
-//     combine(8);
-// }
-// let nineButton = document.querySelector('.nine');
-// nineButton.addEventListener('mousedown', nineFxn)
+function multFxn() {
+    num1 = [joinedNumber, 'operate', 'mult'];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// function nineFxn() {
-//     combine(9);
-// }
+let plusButton = document.querySelector('.plus');
+plusButton.addEventListener('mousedown', plusFxn);
 
-// let fourButton = document.querySelector('.four');
-// fourButton.addEventListener('mousedown', fourFxn)
+function plusFxn() {
+    num1 = [joinedNumber, 'operate', 'plus'];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// function fourFxn() {
-//     combine(4);
-// }
+let minusButton = document.querySelector('.minus');
+minusButton.addEventListener('mousedown', minusFxn);
 
-// let fiveButton = document.querySelector('.five');
-// fiveButton.addEventListener('mousedown', fiveFxn)
+function minusFxn() {
+    num1 = [joinedNumber, 'operate', 'minus'];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// function fiveFxn() {
-//     combine(5);
-// }
 
-// let sixButton = document.querySelector('.six');
-// sixButton.addEventListener('mousedown', sixFxn)
+// ----- EQUALS FUNCTION -----
+let equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('mousedown', equalsFxn);
 
-// function sixFxn() {
-//     combine(6);
-// }
+function equalsFxn() {
+    if (num1[2] === 'divide') {
+        answer = num1[0] / joinedNumber;
+    } else if (num1[2] === 'mult') {
+        answer = num1[0] * joinedNumber;
+    } else if (num1[2] === 'plus') {
+        answer = num1[0] + joinedNumber;
+    } else {
+        answer = num1[0] - joinedNumber;
+    }
+    changeDisplay(answer);
+    console.log(`Answer: ${answer}`)
+    return answer
+}
 
-// let oneButton = document.querySelector('.one');
-// oneButton.addEventListener('mousedown', oneFxn)
+// ----- CLEAR FUNCTION -----
+let clearButton = document.querySelector('.clear');
+clearButton.addEventListener('mousedown', clearFxn);
 
-// function oneFxn() {
-//     combine(1);
-// }
+function clearFxn() {
+    num1 = [0, 'clear', 0];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// let twoButton = document.querySelector('.two');
-// twoButton.addEventListener('mousedown', twoFxn)
+// ---- NEGATIVE FUNCTION ----
+let negButton = document.querySelector('.neg');
+negButton.addEventListener('mousedown', negFxn);
 
-// function twoFxn() {
-//     combine(2);
-// }
+function negFxn() {
+    num1 = [0, 'negative', 0];
+    console.log(num1);
+    combine(0);
+    return num1
+}
 
-// let threeButton = document.querySelector('.three');
-// threeButton.addEventListener('mousedown', threeFxn)
+// ----- DISPLAY NUMBER -----
+let display = document.querySelector('.display');
 
-// function threeFxn() {
-//     combine(3);
-// }
+function changeDisplay(displayText) {
+display.innerText = displayText;
+console.log(display.innerText);
+return display.innerText;
+}
+
+// ----- BUTTONS OUTPUT NUMBERS -----
+let sevenButton = document.querySelector('.seven');
+sevenButton.addEventListener('mousedown', sevenFxn)
+
+function sevenFxn() {
+    combine(7);
+}
+
+let eightButton = document.querySelector('.eight');
+eightButton.addEventListener('mousedown', eightFxn)
+
+function eightFxn() {
+    combine(8);
+}
+let nineButton = document.querySelector('.nine');
+nineButton.addEventListener('mousedown', nineFxn)
+
+function nineFxn() {
+    combine(9);
+}
+
+let fourButton = document.querySelector('.four');
+fourButton.addEventListener('mousedown', fourFxn)
+
+function fourFxn() {
+    combine(4);
+}
+
+let fiveButton = document.querySelector('.five');
+fiveButton.addEventListener('mousedown', fiveFxn)
+
+function fiveFxn() {
+    combine(5);
+}
+
+let sixButton = document.querySelector('.six');
+sixButton.addEventListener('mousedown', sixFxn)
+
+function sixFxn() {
+    combine(6);
+}
+
+let oneButton = document.querySelector('.one');
+oneButton.addEventListener('mousedown', oneFxn)
+
+function oneFxn() {
+    combine(1);
+}
+
+let twoButton = document.querySelector('.two');
+twoButton.addEventListener('mousedown', twoFxn)
+
+function twoFxn() {
+    combine(2);
+}
+
+let threeButton = document.querySelector('.three');
+threeButton.addEventListener('mousedown', threeFxn)
+
+function threeFxn() {
+    combine(3);
+}
+
+let zeroButton = document.querySelector('.zero');
+zeroButton.addEventListener('mousedown', zeroFxn)
+
+function zeroFxn() {
+    combine(0);
+}
+
